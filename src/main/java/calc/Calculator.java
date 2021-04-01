6fcd8931f4ddf7e55bee25af6782075ad3d6c4ef
Arif Hossain <arif.hossain@iths.se>
2021-04-01 10:46:03 +0200

    webhook test

--- src/main/java/calc/Calculator.java
+++ src/main/java/calc/Calculator.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Calculator {
     public static void main ( String[] args ) {
         Scanner sc = new Scanner (System.in);
+        System.out.println ("================");
         System.out.print ("MultiplyTable: ");
 
         int number = sc.nextInt ();
